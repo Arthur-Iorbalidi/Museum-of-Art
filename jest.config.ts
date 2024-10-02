43c9e768b1f604f4930554d8d1bc9b146917f1c2
Arthur Iorbalidi <arturiorbalidi01052005@gmail.com>
2024-10-02 20:58:36 +0300

    test: setup test environment, implement tests for Header and Footer

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+export default {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy',
+    '^.+\\.svg$': 'jest-transformer-svg',
+    '^@assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@constants/(.*)$': '<rootDir>/src/constants/$1',
+    '^@context/(.*)$': '<rootDir>/src/context/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+  },
+};
